251d5942aa54b62b198f8db3191c588e9240adc3
kalilamali <kalilamali@hotmail.com>
2019-02-01 00:53:56 +0100

    Linter correction on dataPlot.py and dataStatistics.py

--- dataStatistics.py
+++ dataStatistics.py
@@ -2,22 +2,22 @@ import numpy as np
 
 
 def dataStatistics(data, statistic):
-    """ DATASTATISTICS calculates 1 out of 7 possible statistics for the data. """
+    """ DATASTATISTICS calculates 1 out of 7 possible statistics for the
+    data. """
 
     # Usage: result = dataStatistics(data, statistic)
     #
-    # Input         data            N x 3 (matrix), where N is number of rows 
+    # Input         data            N x 3 (matrix), where N is number of rows
     #               statistic       to be calculated (string), e.g. "Rows"
     # Output        result          result of calculated statistic (scalar)
     #
     # Author: Karen Loaiza, s181423@student.dtu.dk, 2018
 
-    # Sort the data by columns: 
+    # Sort the data by columns:
     # (T) Temperature, (G) Growth rate, and (B) Bacteria.
-    T = data[:,0]
-    G = data [:,1]
-    B = data [:,2]
-    
+    T = data[:, 0]
+    G = data[:, 1]
+
     # Calculate the corresponding statistic on the sorted columns.
     if statistic == "Mean Temperature":  # Average T.
         result = np.mean(T)
@@ -29,9 +29,8 @@ def dataStatistics(data, statistic):
         result = np.std(G)
     elif statistic == "Rows":  # Total number of rows in data.
         result = len(data)
-    elif statistic == "Mean Cold Growth rate":  # Average G when T < 20 degrees.
-        result = np.mean(T[T<20])
-    elif statistic == "Mean Hot Growth rate":  # Average G when T > 50 degrees.
-        result = np.mean(T[T>50])
+    elif statistic == "Mean Cold Growth rate":  # Average G when T < 20degrees.
+        result = np.mean(T[T < 20])
+    elif statistic == "Mean Hot Growth rate":  # Average G when T > 50degrees.
+        result = np.mean(T[T > 50])
     return result
-
