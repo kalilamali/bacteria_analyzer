7a2b01968ed0386e4c30faaaec645d0f83ea5040
kalilamali <kalilamali@hotmail.com>
2019-02-01 00:48:15 +0100

    Linter correction on bDetection.py

--- bDetection.py
+++ bDetection.py
@@ -7,20 +7,20 @@ def bDetection(data):
 
     # Usage: bDectection(data)
     #
-    # Input         data            N x M (matrix), where N is number of rows 
+    # Input         data            N x M (matrix), where N is number of rows
     # Output        on screen       message, e.g. Data contains 11 Listeria
     #
     # Author: Karen Loaiza, s181423@student.dtu.dk, 2018
 
     # Create an array of custom bacteria names.
     b_name = np.array(["Salmonella enterica.",
-                       "Bacillus cereus.", 
+                       "Bacillus cereus.",
                        "Listeria.",
                        "Brochothrix thermosphacta."])
-    # Data has 3 columns: 0, 1, 2. 
+    # Data has 3 columns: 0, 1, 2.
     # Extract the bacteria column as an array and convert it to a list.
     b_list = list(map(int, data[:, 2]))
-    # Count the bacteria type ocurrences and print a message with the 
+    # Count the bacteria type ocurrences and print a message with the
     # bacteria's name.
     for i in range(len(b_name)):
         count = b_list.count(i+1)
